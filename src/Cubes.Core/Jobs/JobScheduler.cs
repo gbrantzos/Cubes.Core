5c83b39752a6261e256aef8b63fa59f7ab44cd47
Giorgos Brantzos <g.brantzos@gmail.com>
2019-03-12 16:03:51 +0200

    Work on JobScheduler

--- src/Cubes.Core/Jobs/JobScheduler.cs
+++ src/Cubes.Core/Jobs/JobScheduler.cs
@@ -1,28 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Cubes.Core.Settings;
+using Cubes.Core.Utilities;
+using Microsoft.Extensions.Logging;
 using Quartz;
 
 namespace Cubes.Core.Jobs
 {
-    public class JobScheduler : IJobScheduler
+    public class JobScheduler : IJobScheduler, IDisposable
     {
+        public const string PARAMETERS_KEY = "ExecutionParameters";
+
+
         private readonly ISettingsProvider settingsProvider;
         private readonly List<(JobDefinition definition, JobExecution execution)> jobDetails;
         private readonly IScheduler quartzScheduler;
+        private readonly ILogger<JobScheduler> logger;
+        private readonly ITypeResolver typeResolver;
 
-        public JobScheduler(ISettingsProvider settingsProvider, IScheduler quartzScheduler)
+        public JobScheduler(ISettingsProvider settingsProvider, IScheduler quartzScheduler, ILogger<JobScheduler> logger, ITypeResolver typeResolver)
         {
             this.settingsProvider = settingsProvider;
             this.quartzScheduler = quartzScheduler;
-
+            this.logger = logger;
+            this.typeResolver = typeResolver;
             this.jobDetails = new List<(JobDefinition definition, JobExecution execution)>();
             this.LoadJobs();
         }
 
         public SchedulerStatus GetStatus()
         {
-            throw new System.NotImplementedException();
+            var schedulerStatus = new SchedulerStatus
+            {
+                State = quartzScheduler.InStandbyMode ? SchedulerState.Stopped : SchedulerState.Started,
+                ServerTime = DateTime.Now,
+            };
+            return schedulerStatus;
         }
 
         public void LoadJobs()
@@ -33,7 +47,7 @@ namespace Cubes.Core.Jobs
 
             if (jobs.Count == 0)
             {
-                // Log end exit
+                logger.LogWarning("No jobs defined, will not start JobScheduler!");
                 return;
             }
 
@@ -46,14 +60,77 @@ namespace Cubes.Core.Jobs
 
         public SchedulerStatus StartScheduler()
         {
-            quartzScheduler.Start();
-            return null;
+            LoadJobs();
+            var activeJobs = jobDetails.Where(i => i.definition.IsActive).ToList();
+            if (activeJobs.Count == 0)
+            {
+                logger.LogWarning("No active jobs defined, JobScheduler has nothing to do!");
+            }
+            else
+            {
+                foreach (var job in activeJobs)
+                {
+                    var trigger = TriggerBuilder
+                        .Create()
+                        .WithCronSchedule(job.definition.CronExpression, i =>
+                            {
+                                if (job.definition.FireIfMissed)
+                                    i.WithMisfireHandlingInstructionFireAndProceed();
+                                else
+                                    i.WithMisfireHandlingInstructionDoNothing();
+                            })
+                        .StartNow();
+                    var jobBuilder = JobBuilder
+                        .Create(typeResolver.GetByName("job.JobTypeName"))
+                        .WithIdentity(job.definition.ID.ToString(), "Cubes")
+                        .WithDescription(job.definition.Description);
+                    if (!String.IsNullOrEmpty(job.definition.ExecutionParameters))
+                        jobBuilder.UsingJobData(PARAMETERS_KEY, job.definition.ExecutionParameters);
+
+                    var scheduledJob = jobBuilder.Build();
+                    quartzScheduler.ScheduleJob(scheduledJob, trigger.Build());
+                    if (!job.definition.IsActive)
+                        quartzScheduler.PauseJob(scheduledJob.Key);
+                }
+                quartzScheduler.Clear();
+                quartzScheduler.Start();
+            }
+            return GetStatus();
         }
 
         public SchedulerStatus StopScheduler()
         {
-            quartzScheduler.Shutdown();
-            return null;
+            if (quartzScheduler.IsStarted)
+            {
+                logger.LogInformation("Stoping JobScheduler.");
+                quartzScheduler.Standby();
+            }
+            return GetStatus();
         }
+
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (quartzScheduler != null)
+                        quartzScheduler.Shutdown();
+                }
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
     }
 }
