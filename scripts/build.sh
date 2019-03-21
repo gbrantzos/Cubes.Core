92f51b6cfb59b9b639403b98fba2ef7e2112fc5a
Giorgos Brantzos <g.brantzos@gmail.com>
2019-03-21 22:49:11 +0200

    Added: Make changes on build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,20 +7,26 @@ HASH=${HASH:0:10}
 DIRECTORY=$(cd `dirname $0` && pwd)
 echo $DIRECTORY
 
+VERSION=5.0.1
+
 echo "Preparing deploy for Git commit: $HASH"
 echo "Working directory: $DIRECTORY"
 cd $DIRECTORY
 
 # Build
-dotnet build ../src/Cubes.Core/Cubes.Core.csproj /p:InformationalVersion=5.0.1-$HASH
-dotnet build ../src/Cubes.Api/Cubes.Api.csproj /p:InformationalVersion=5.0.1-$HASH
-dotnet build ../src/Cubes.Host/Cubes.Host.csproj /p:InformationalVersion=5.0.1-$HASH
+dotnet clean -c release ../src/Cubes.Core/Cubes.Core.csproj
+dotnet build -c release ../src/Cubes.Core/Cubes.Core.csproj -p:VersionPrefix=$VERSION --version-suffix "$HASH" -p:InformationalVersion=$VERSION-$HASH
+dotnet clean -c release ../src/Cubes.Api/Cubes.Api.csproj
+dotnet build -c release ../src/Cubes.Api/Cubes.Api.csproj -p:VersionPrefix=$VERSION --version-suffix "$HASH" -p:InformationalVersion=$VERSION-$HASH
+dotnet clean -c release ../src/Cubes.Host/Cubes.Host.csproj
+dotnet build -c release ../src/Cubes.Host/Cubes.Host.csproj -p:VersionPrefix=$VERSION --version-suffix "$HASH" -p:InformationalVersion=$VERSION-$HASH
+
 
 # Publish
 mkdir ../tmp
 cd ../tmp
 rm -rf *
-dotnet publish ../src/Cubes.Host/Cubes.Host.csproj -o $DIRECTORY/../tmp/Cubes-$HASH -c Release
+dotnet publish --no-build ../src/Cubes.Host/Cubes.Host.csproj -o $DIRECTORY/../tmp/Cubes-$HASH -c release -p:VersionPrefix=$VERSION --version-suffix "$HASH"
 
 # Create zip
 zip -r Cubes-$HASH.zip .
@@ -28,4 +34,3 @@ mv Cubes-$HASH.zip ../deploy
 
 # Cleanup
 rm -rf ../tmp
-
