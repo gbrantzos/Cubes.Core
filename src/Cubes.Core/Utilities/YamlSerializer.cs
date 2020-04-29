865e88577fe5008ed321623c0e086f8e730d5e21
Giorgos Brantzos <g.brantzos@gmail.com>
2020-04-29 14:57:06 +0300

    Fix: YAML serialization - strings
    Improve YAML serialization for strings, avoid empty lines issue

--- src/Cubes.Core/Utilities/YamlSerializer.cs
+++ src/Cubes.Core/Utilities/YamlSerializer.cs
@@ -1,12 +1,16 @@
 using System;
 using Cubes.Core.Base;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.EventEmitters;
 
 namespace Cubes.Core.Utilities
 {
     public class YamlSerializer : ISerializer
     {
         private static YamlDotNet.Serialization.ISerializer Serializer { get; } = new SerializerBuilder()
+            .WithEventEmitter(next => new StringHandlerEmitter(next))
             .Build();
         private static IDeserializer Deserializer { get; } = new DeserializerBuilder()
             .Build();
@@ -21,21 +25,21 @@ namespace Cubes.Core.Utilities
     }
 }
 
-//public class MultilineStringEmitter : ChainedEventEmitter
-//{
-//    public MultilineStringEmitter(IEventEmitter nextEmitter) : base(nextEmitter) { }
-//    public override void Emit(ScalarEventInfo eventInfo, IEmitter emitter)
-//    {
-//        if (eventInfo.Source.Type == typeof(string))
-//        {
-//            var value = (string)eventInfo.Source.Value;
-//            if (!String.IsNullOrEmpty(value) && value.Contains('\n'))
-//            {
-//                var ev = new Scalar(null, null, value, ScalarStyle.Literal, true, false);
-//                emitter.Emit(ev);
-//                return;
-//            }
-//        }
-//        base.Emit(eventInfo, emitter);
-//    }
-//}
+public class StringHandlerEmitter : ChainedEventEmitter
+{
+    public StringHandlerEmitter(IEventEmitter nextEmitter) : base(nextEmitter) { }
+    public override void Emit(ScalarEventInfo eventInfo, IEmitter emitter)
+    {
+        if (eventInfo.Source.Type == typeof(string))
+        {
+            var value = (string)eventInfo.Source.Value;
+            if (!String.IsNullOrEmpty(value) && value.Contains('\n'))
+            {
+                var ev = new Scalar(null, null, value, ScalarStyle.Literal, true, false);
+                emitter.Emit(ev);
+                return;
+            }
+        }
+        base.Emit(eventInfo, emitter);
+    }
+}
