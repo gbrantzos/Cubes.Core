2d778455ef3680c6972a0458711a138a7e9ea484
Giorgos Brantzos <g.brantzos@gmail.com>
2020-10-24 19:14:07 +0300

    Environment fixes

--- src/Cubes.Core/BuildInfo.ps1
+++ src/Cubes.Core/BuildInfo.ps1
@@ -1,20 +1,6 @@
 # Output file
 $OutFile = "BuildInfo.txt"
 
-# Build number
-$BuildNumber = 0
-Try
-{
-    $NumberLine = (Get-Content $OutFile | Select-String -Pattern 'Build').Line
-    $separatorIndex = $NumberLine.IndexOf(':') + 1
-    $BuildNumber = [int]($NumberLine.Substring($separatorIndex).Trim())
-    $BuildNumber = $BuildNumber + 1
-}
-Catch
-{
-    Write-Output "No build information found!"
-}
-
 # Git details
 $GitBranch = (git rev-parse --abbrev-ref HEAD)
 $GitCommit = (git rev-parse HEAD).Substring(0, 10)
@@ -24,4 +10,3 @@ Get-Date -Format 'yyyy/MM/dd HH:mm:ss' | Out-File -FilePath $OutFile
 Write-Output '' | Out-File -FilePath $OutFile -Append
 Write-Output "Branch: $GitBranch"   | Out-File -FilePath $OutFile -Append
 Write-Output "Commit: $GitCommit"   | Out-File -FilePath $OutFile -Append
-Write-Output "Build : $BuildNumber" | Out-File -FilePath $OutFile -Append
