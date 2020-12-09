4ef5404076de45b8a3925ed247196860132853a2
Giorgos Brantzos <g.brantzos@gmail.com>
2020-12-09 21:50:03 +0200

    Improve bumpVersion script

--- scripts/BumpVersion.ps1
+++ scripts/BumpVersion.ps1
@@ -27,6 +27,35 @@ $tokens = @(
     'Version'
 )
 
+function Check-TagExistence {
+    $status =  (git tag -l "v$Version")
+      
+    if (![string]::IsNullOrWhitespace($status)) 
+    { 
+        Write-Host  "Tag 'v$Version' already exsists in repository!`nAborting..."
+        exit 1
+    }
+}
+
+function Pending-Changes {
+    # Check for GIT pending changes
+    $st = (git status -su)
+    if (![string]::ISNullOrEmpty($st)) {
+        Write-Host "Git repository has pending changes!`nAborting..."
+        exit 3
+    }
+}
+
+function Validate-Version {
+    # Check version format
+    $re=[regex]"([0-9]\.[0-9]\.[0-9])(\-){0,1}((.)*)"
+    $m=$re.Match($Version)
+    if (!$m.Success) {
+        Write-Host "Invalid version format! $Version!`nAborting..."
+        exit 2
+    }
+}
+
 function Process-File {
     param ([string] $file)
 
@@ -45,7 +74,20 @@ function Process-File {
 
 }
 
+function Bump-VersionAndTag {
+    # Add and commit
+    git add --all
+    git commit -m "Bump version $version"
+    git tag -a "v$version" -m "Version $version"
+}
+
+Validate-Version
+Check-TagExistence
+Pending-Changes
+
 Process-File Cubes.Core/Cubes.Core.csproj
 Process-File Cubes.Host/Cubes.Host.csproj
+
+Bump-VersionAndTag
 
 # ------------------------------------------------------------------------------
